87fd1eee114edef50e59d1af105680515cb39f1e
Yeiner Morales <yeiner.morales@gmail.com>
2017-07-19 17:08:21 -0500

    Adelanto rombo

---
+++ ejercicios/rombo.cpp
@@ -0,0 +1,37 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  rombo.cpp
+ *
+ *    Description:  Rombo en c++
+ *
+ *        Version:  1.0
+ *        Created:  19/07/17 14:56:47
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "   * " << endl;
+    cout << "  *** " << endl;
+    cout << " ***** " << endl;
+    cout << "  *** " << endl;
+    cout << "   * " << endl;
+    
+    for(i =10; i >0 ; i--){
+        
+        cout << "*" << endl;
+    }
+    
+    return 0;
+}
+
