4fc3e4e1de611ff9a9edabc2195ff9cc18cf8702
Yeiner Morales <yeiner.morales@gmail.com>
2018-02-21 17:40:32 -0500

    Adelanto programa minimo común multiplo

---
+++ ejercicios/varios/minimoComunMultiplo.cpp
@@ -0,0 +1,39 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  minimoComunMultiplo.cpp
+ *
+ *    Description:  : Encontrar el minimo comun multiplo de 3 numeros.
+ *
+ *        Version:  1.0
+ *        Created:  21/02/18 15:53:04
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Yeiner Morales (@yeiner_morales), 
+ *   Organization:  @yeiner
+ *
+ * =====================================================================================
+ */
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int numOne;
+    int numTwo;
+    int numThree;
+   
+    cout<<"Ingrese numero uno :"<< endl;
+    cin >> numOne; 
+    cout << "Ingrese numero dos : " << endl;
+    cin >> numTwo; 
+    cout << "Ingrese numero tres : "  << endl;
+    cin >> numThree; 
+    
+    
+
+    return 0;
+}
+
---
+++ ejercicios/votos.cpp
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  votos.cpp
+ *
+ *    Description:  :
+ *
+ *        Version:  1.0
+ *        Created:  20/09/17 14:21:01
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+#include <iostream>
+
+using namespace std; 
+
+int main() {
+
+
+    return 0;
+}
+
