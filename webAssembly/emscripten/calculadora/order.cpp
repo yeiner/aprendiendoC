0cd8fb004a8bf0125fa62b6f9e7ea794a44495f5
Yeiner Morales <yeiner.morales@gmail.com>
2020-07-31 19:16:05 -0500

    Aprendiendo webassembly

---
+++ webAssembly/emscripten/calculadora/order.cpp
@@ -0,0 +1,20 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  order.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  07/07/20 20:30:01
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Yeiner Morales (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <emscripten.h>
+
+EMSCRIPTEN_KEEPALIVE
---
+++ webAssembly/emscripten/tools-finance/order.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  order.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  07/07/20 20:36:27
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Yeiner Morales (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <emscripten.h>
+#include <math.h>
+
+EMSCRIPTEN_KEEPALIVE 
+float valor_cuota(float interest, float capital, float term){
+    return capital * interest / (100 * (1 - pow((1 + interest / 100), - term)));
+}
+
